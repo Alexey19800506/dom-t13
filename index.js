bde8013aa75b07ce58619b9df4805dcdea5a1fa4
Alexey19800506 <104822703+Alexey19800506@users.noreply.github.com>
2022-07-07 10:37:50 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 export function getSection(num) {
    const elem = document.querySelector(`span[data-number = "${num}"]`);
-   return elem.closest('.box').dataset;
+   return elem.closest('.box').dataset.section;
 }
